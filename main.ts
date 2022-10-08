7874213e1d203a82df1d9059fed78562567eacec
Napat0000 <88538930+Napat0000@users.noreply.github.com>
2022-10-08 17:01:20 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,84 @@
+function Touchedge () {
+    if (Phead.get(LedSpriteProperty.Direction) == 0 && Phead.get(LedSpriteProperty.Y) == 0) {
+        Death()
+    }
+    if (Phead.get(LedSpriteProperty.Direction) == 90 && Phead.get(LedSpriteProperty.X) == 4) {
+        Death()
+    }
+    if (Phead.get(LedSpriteProperty.Direction) == 180 && Phead.get(LedSpriteProperty.Y) == 4) {
+        Death()
+    }
+    if (Phead.get(LedSpriteProperty.Direction) == 270 && Phead.get(LedSpriteProperty.X) == 0) {
+        Death()
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    Phead.turn(Direction.Left, 90)
+})
+function Death () {
+    game.setScore(Score)
+    game.gameOver()
+}
+input.onButtonPressed(Button.B, function () {
+    Phead.turn(Direction.Right, 90)
+})
+let Score = 0
+let Phead: game.LedSprite = null
+Phead = game.createSprite(1, 0)
+let Pbody = [game.createSprite(0, 0)]
+let Pbx: number[] = []
+let Pby: number[] = []
+let PBdir: number[] = []
+let Eated = false
+let Snakelength = 1
+let Snakespeed = 1000
+Score = 0
+game.setScore(0)
+let Food = game.createSprite(randint(0, 4), randint(0, 4))
+Food.set(LedSpriteProperty.Blink, 100)
+Pbx.unshift(Phead.get(LedSpriteProperty.X))
+Pby.unshift(Phead.get(LedSpriteProperty.Y))
+PBdir.unshift(Phead.get(LedSpriteProperty.Direction))
+basic.forever(function () {
+    for (let index = 0; index <= Snakelength; index++) {
+        if (index > 0) {
+            Pbody[index - 1].set(LedSpriteProperty.X, Pbx[index])
+            Pbody[index - 1].set(LedSpriteProperty.Y, Pby[index])
+            Pbody[index - 1].set(LedSpriteProperty.Brightness, 100)
+            if (Pbody[index - 1].isTouching(Food)) {
+                Food.set(LedSpriteProperty.X, randint(0, 4))
+                Food.set(LedSpriteProperty.Y, randint(0, 4))
+            }
+            if (Snakelength == 20) {
+                Pbody[index - 1].delete()
+            }
+            if (Pbody[index - 1].isTouching(Phead)) {
+                Death()
+            }
+        }
+    }
+})
+basic.forever(function () {
+    Phead.move(1)
+    Pbx.unshift(Phead.get(LedSpriteProperty.X))
+    Pby.unshift(Phead.get(LedSpriteProperty.Y))
+    PBdir.unshift(Phead.get(LedSpriteProperty.Direction))
+    if (Phead.isTouching(Food)) {
+        Food.delete()
+        Food = game.createSprite(randint(0, 4), randint(0, 4))
+        Food.set(LedSpriteProperty.Blink, 100)
+        Score += 1
+        Snakelength += 1
+        if (Snakelength == 20) {
+            basic.pause(2000)
+            Pbody = [game.createSprite(0, 0)]
+            Score = Score + randint(2, 5)
+            Snakespeed += -100
+            Snakelength = 1
+        } else {
+            Pbody.unshift(game.createSprite(Pbx[Snakelength], Pby[Snakelength]))
+        }
+    }
+    basic.pause(Snakespeed)
+    Touchedge()
+})
